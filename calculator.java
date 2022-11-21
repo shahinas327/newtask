87a6f1097f48e3c25530a0df258d12f2ad4316db
shahinas327 <112380003+shahinas327@users.noreply.github.com>
2022-11-21 10:55:48 +0530

    first commit

---
+++ calculator.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class calculator
+{
+    public static void main(String args[])
+    {
+
+        int num1 = 2, num2 = 5, option;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the choice");
+        System.out.println("1. Addition");
+        option = sc.nextInt();
+        switch(option)
+        {
+            case 1:
+                System.out.println("Addition of " + num1 + " and " + num2 + " is " + (num1 + num2));
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
+    }
+}
